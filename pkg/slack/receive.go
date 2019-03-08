be07ec3523b2c0240db4ca2c8b675dec10a71ae1
Scott Nichols <nicholss@google.com>
2019-03-08 08:27:49 -0800

    minor format change

--- pkg/slack/receive.go
+++ pkg/slack/receive.go
@@ -14,9 +14,12 @@ func (s *Slack) cloudEventReceiver(event cloudevents.Event) {
 }
 
 func (s *Slack) doResponse(event cloudevents.Event) {
-	resp := events.Message{}
-	if err := event.DataAs(&resp); err != nil {
-		s.Err <- fmt.Errorf("failed to get data from cloudevent %s", event.String())
-	}
-	s.rtm.SendMessage(s.rtm.NewOutgoingMessage(resp.Text, resp.Channel))
+	// don't block the cloudevents client.
+	go func() {
+		resp := events.Message{}
+		if err := event.DataAs(&resp); err != nil {
+			s.Err <- fmt.Errorf("failed to get data from cloudevent %s", event.String())
+		}
+		s.rtm.SendMessage(s.rtm.NewOutgoingMessage(resp.Text, resp.Channel))
+	}()
 }
